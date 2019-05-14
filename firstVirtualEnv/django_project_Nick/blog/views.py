d69dc40b260c7f10ba5a702c43a167e0ebc544d5
Ni Tongyang <ntyaiqingr@gmail.com>
2019-05-14 00:40:36 -0400

    apply template to interface

--- firstVirtualEnv/django_project_Nick/blog/views.py
+++ firstVirtualEnv/django_project_Nick/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Nick',
+        'title': 'Blog post1',
+        'content': 'First post content',
+        'date_posted': '05/13/2019'
+    },
+    {
+        'author': 'Tongyang',
+        'title': 'Blog post2',
+        'content': 'Second post content',
+        'date_posted': '05/14/2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
