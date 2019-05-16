32a357efe35bd94d0d2deadbd419bf341ebb0604
Ni Tongyang <ntyaiqingr@gmail.com>
2019-05-15 23:36:23 -0400

    post data and get data from post

--- firstVirtualEnv/django_project_Nick/blog/views.py
+++ firstVirtualEnv/django_project_Nick/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Nick',
-        'title': 'Blog post1',
-        'content': 'First post content',
-        'date_posted': '05/13/2019'
-    },
-    {
-        'author': 'Tongyang',
-        'title': 'Blog post2',
-        'content': 'Second post content',
-        'date_posted': '05/14/2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
