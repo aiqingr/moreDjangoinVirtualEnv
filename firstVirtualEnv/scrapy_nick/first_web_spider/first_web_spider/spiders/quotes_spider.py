938450be6b91c381a2263fe52ff6e3bd5cedd794
Ni Tongyang <ntyaiqingr@gmail.com>
2019-05-22 00:35:19 -0400

    more css selector

--- firstVirtualEnv/scrapy_nick/first_web_spider/first_web_spider/spiders/quotes_spider.py
+++ firstVirtualEnv/scrapy_nick/first_web_spider/first_web_spider/spiders/quotes_spider.py
@@ -8,5 +8,12 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        title = response.css('title').extract()
-        yield {'titlecontext': title}
+        all_div_quotes = response.css('div.quote')[0]
+        title = all_div_quotes.css('span.text::text').extract()
+        author = all_div_quotes.css('.author::text').extract()
+        tag = all_div_quotes.css('.tag::text').extract()
+        yield {
+            'title': title,
+            'author': author,
+            'tag': tag
+        }
