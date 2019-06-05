1218a4e37306509fa527e5e8176a2e4e7fd205f7
Ni Tongyang <ntyaiqingr@gmail.com>
2019-06-04 23:53:44 -0400

    binary mode for a file object

--- firstVirtualEnv/libraryLearning/file_object/file_object_write.py
+++ firstVirtualEnv/libraryLearning/file_object/file_object_write.py
@@ -5,7 +5,17 @@
 #     f.seek(0)
 #     f.write('R')
 
-with open('testing.txt', 'r') as rf:
-    with open('testing_copy.txt', 'w') as wf:
-        for line in rf:
-            wf.write(line)
+# The way to copy content to a new doc
+# with open('testing.txt', 'r') as rf:
+#     with open('testing_copy.txt', 'w') as wf:
+#         for line in rf:
+#             wf.write(line)
+
+# If want to copy an image, need to use binary mode
+with open('HD.jpeg', 'rb') as rf:
+    with open('HD_1.jpeg', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
