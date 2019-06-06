6cb004f9e6a8840019322a7088899a8a0e8b0fa8
Ni Tongyang <ntyaiqingr@gmail.com>
2019-06-06 00:08:54 -0400

    pass data or parameter when get/post method

--- firstVirtualEnv/libraryLearning/library_requests/library_requests.py
+++ firstVirtualEnv/libraryLearning/library_requests/library_requests.py
@@ -1,6 +1,11 @@
 import requests
 
-r = requests.get('https://imgs.xkcd.com/comics/python.png')
+payload = {'username': 'TNI', 'password': 'testing'}
+r = requests.post('https://httpbin.org/post', data=payload)
 
-with open('comic.png', 'wb') as f:
-    f.write(r.content)
+# with open('comic.png', 'wb') as f:
+#     f.write(r.content)
+
+r_dict = r.json()
+
+print(r_dict['form'])
